948b2d16389dffd3ffe8a7476927ac4f69c6def9
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2025-12-26 23:09:24 +0600

    complete first basic theory

---
+++ module1/src/1.12.ts
@@ -0,0 +1,49 @@
+{
+
+    // nullable types
+
+    const searchName = (value: string | null) => {
+        if(value){
+            console.log('Searching...');
+        }else{
+            console.log('There is nothing to search');
+        }
+    }
+    searchName(null)
+
+    //unknown type
+    const getSpeedInMeterPerSecond = (value: unknown) => {
+        if(typeof value == 'number'){
+            const convertedSpeed = (value * 1000)/3600;
+            console.log(`This speed is ${convertedSpeed} ms^-1`);
+        }
+
+        else if(typeof value == 'string'){
+            const [result, unit] = value.split(' ');
+            const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+            // console.log(result);
+            console.log(`The speed is ${convertedSpeed} ms^-1`);
+        }
+        else{
+            console.log('wrong input');
+        }
+    }
+    // getSpeedInMeterPerSecond(1000)
+    // getSpeedInMeterPerSecond(`1000 kmh^-1`)
+    getSpeedInMeterPerSecond(null)
+
+    //never type 
+    // const throwError = (msg: string) : never => {
+    //     throw new Error(msg)
+    // }
+    // throwError('muslik se error hoyece')
+
+
+
+
+
+
+
+
+
+}
